eee0912e4bd73f6eea853e347340cd7328ed8c19
Artorias2718 <arturoordonez271@gmail.com>
2019-05-01 21:00:42 -0600

    I've created a shared library containing a sample data store for a mobile application.

---
+++ TrainingRooms/RoomRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrainingRooms
+{
+    public class RoomRepository
+    {
+        private List<TrainingRoom> m_rooms = new List<TrainingRoom>
+        {
+            new TrainingRoom
+            {
+                Id = 1,
+                Name = "Copernicus",
+                Location = "bldg 1",
+                NumberComputers = 25
+            },
+            new TrainingRoom
+            {
+                Id = 2,
+                Name = "Sagitarius",
+                Location = "bldg 1",
+                NumberComputers = 50
+            },
+            new TrainingRoom
+            {
+                Id = 3,
+                Name = "Luna",
+                Location = "bldg 1",
+                NumberComputers = 75
+            }
+        };
+
+        public List<TrainingRoom> GetRooms()
+        {
+            return m_rooms;
+        }
+
+        public TrainingRoom GetRoom(int id)
+        {
+            return (from r in m_rooms where r.Id == id select r).FirstOrDefault();
+        }
+    }
+}
