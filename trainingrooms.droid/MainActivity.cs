0fcde982c2e6ca43aa97610b3e32fbf79ea59274
Arturo Ordonez <arturoordonez@weber.edu>
2019-05-03 13:48:05 -0600

    I've setup a few layout files to separate the overall application layout with our content layout.

--- trainingrooms.droid/MainActivity.cs
+++ trainingrooms.droid/MainActivity.cs
@@ -17,7 +17,7 @@ namespace trainingrooms.droid
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.Main);
 
             // Get our button from the layout resource
             // and attach an event to it
